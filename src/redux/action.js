32d4aa694ff654ea90a0714292d37a45138ea553
Victor1988V <110904832+Victor1988V@users.noreply.github.com>
2023-01-18 20:35:31 +0000

    Deploying to gh-pages from @ Victor1988V/goit-react-hw-06-phonebook@bda73f0756ab0d1b57a70dcfebe6202c9be7f108 🚀

--- src/redux/action.js
+++
@@ -1,9 +0,0 @@
-import { ADD_USER, FILTER } from './actionTypes';
-
-export const addUser = payload => {
-  return { type: ADD_USER, payload: { ...payload } };
-};
-
-export const filter = payload => {
-  return { type: FILTER, payload };
-};
